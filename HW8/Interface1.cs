b3822b37f6d0c4f5bf7a3bfbced34b1b96e7c055
Spaceecs <azaza2046goi@gmail.com>
2024-01-19 22:29:10 +0200

    Добавьте файлы проекта.

---
+++ HW8/Interface1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8
+{
+    public interface IPart
+    {
+        public void BuildThis();
+    }
+    public interface IWorker
+    {
+        public void Work();
+    }
+    public class Basemante:IPart
+    {
+        public bool Built { get; set; }
+        public Basemante(bool built)
+        {
+            Built = built;
+        }
+        public Basemante() { Built = false; }
+        public void BuildThis()
+        {
+            Built = true;
+        }
+    }
+}
